2301c11c4d2b8f1c1a736581335e3fcf7b31a706
Igor Lunyakin <lunyaigor484@gmail.com>
2023-07-31 16:33:59 +0300

    hw-10

---
+++ src/index.js
@@ -0,0 +1,80 @@
+import Notiflix from "notiflix";
+import SlimSelect from "slim-select";
+import "slim-select/dist/slimselect.css";
+import axios from "axios";
+import { fetchBreeds, fetchCatByBreed, fetchCatInfoByBreed } from "./js/cat-api";
+
+const API_KEY = 'live_u32GosH3z96t27To0vi83OFNB6w58fS7IkjhSBAK7LN7pTgvLyAXIDwQCGgyaWbP';
+
+axios.defaults.headers.common["x-api-key"] = API_KEY;
+
+const refs = {
+    select: document.querySelector(".breed-select"),
+    loader: document.querySelector(".loader"),
+    error: document.querySelector(".error"),
+    catInfo: document.querySelector(".cat-info"),
+};
+
+hideSelector();
+fetchBreeds().then(breeds => {
+    renderBreedsSelectMarkup(breeds);
+    hideLoader();
+    hideSelector();
+}).catch(error => console.log(error));
+
+refs.select.addEventListener("change", () => {
+    hideLoader();
+
+    fetchCatByBreed(refs.select.value).then(breed => {
+        renderCatImageMarkup(breed);
+        fetchCatInfoByBreed(refs.select.value).then(renderCatInfoMarkup).catch(error => console.log(error));
+        hideLoader();
+        // console.log(breed);
+    }).catch(error => {
+        console.log(error);
+        hideLoader();
+        showError();
+        hideSelector();
+    });
+});
+
+function renderBreedsSelectMarkup(breeds) {
+    const markup = breeds.map(({id, name}) => {
+        return `<option value="${id}">${name}</option>`;
+    }).join();
+
+    refs.select.innerHTML = markup;
+
+    const slimSelect = new SlimSelect({
+        select: '#breed-select'
+    });
+}
+
+function renderCatImageMarkup([breed]) {
+    const markup =  `<img src="${breed.url}">`;
+
+    refs.catInfo.innerHTML = '';
+    refs.catInfo.insertAdjacentHTML("beforeend", markup);
+}
+
+function renderCatInfoMarkup({name, description, temperament}) {
+    const markup = `<div>
+    <h1>${name}</h1>
+    <p>${description}</p>
+    <p><b>Temperament: </b>${temperament}</p>
+    </div>`
+
+    refs.catInfo.insertAdjacentHTML("beforeend", markup);
+}
+
+function hideLoader() {
+    refs.loader.hidden = !refs.loader.hidden;
+}
+
+function hideSelector() {
+    refs.select.hidden = !refs.select.hidden;
+}
+
+function showError() {
+    Notiflix.Report.failure('Error', 'Oops! Something went wrong! Try reloading the page!');
+} 
---
+++ src/js/cat-api.js
@@ -0,0 +1,31 @@
+function fetchBreeds() {
+    return fetch('https://api.thecatapi.com/v1/breeds').then(response => {
+        if(!response.ok) {
+            throw new Error(response.status);
+        }
+
+        return response.json();
+    });
+}
+
+function fetchCatByBreed(breedId) {
+    return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`).then(response => {
+        if(!response.ok) {
+            throw new Error(response.status);
+        }
+
+        return response.json();
+    });
+}
+
+function fetchCatInfoByBreed(breedId) {
+    return fetch(`https://api.thecatapi.com/v1/breeds/${breedId}`).then(response => {
+        if(!response.ok) {
+            throw new Error(response.status);
+        }
+
+        return response.json();
+    });
+}
+
+export {fetchBreeds, fetchCatByBreed, fetchCatInfoByBreed};
